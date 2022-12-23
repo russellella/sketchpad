64c6b6e3c8b02938d4694a4a444773804c901346
russellella <116303658+russellella@users.noreply.github.com>
2022-12-23 05:14:09 -0800

    Add create grid with function
    -Added javascript to create a grid that fits within the div with a
    function, allowing for variable width/height.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
 
-// Create a div
-const container = document.querySelector("#sketchpage");
+let x = 4
+let size = 500/x;
 
-const content = document.createElement("div");
-container.appendChild(content);
+for (let i = 0; i < (x*x); i++) {
+    const container = document.querySelector("#sketchpage");
+    const content = document.createElement("div");
+    content.classList.add("dot");
+    content.textContent = "";
+    content.style.height = size + "px";
+    content.style.width = size + "px";
+    container.appendChild(content);
+}
+
+
